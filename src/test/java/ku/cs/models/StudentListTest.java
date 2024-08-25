b32610a36a0621832b7e93e9f5823cc4f972106e
ariyachai <ariyachai.jom@gmail.com>
2024-08-26 00:20:32 +0700

    update

--- src/test/java/ku/cs/models/StudentListTest.java
+++ src/test/java/ku/cs/models/StudentListTest.java
@@ -1,7 +1,66 @@
 package ku.cs.models;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class StudentListTest {
+
+public class StudentListTest {
+
+    private StudentList studentList;
+
+    @BeforeEach
+    void setUp() {
+        studentList = new StudentList();
+    }
+
+    @Test
+    void testAddNewStudent2Parameters() {
+        studentList.addNewStudent("6610451249", "Ariyachai");
+        Student student = studentList.findStudentById("6610451249");
+        assertNotNull(student);
+        assertEquals("Ariyachai", student.getName());
+    }
+
+    @Test
+    void testAddNewStudent3Parameters() {
+        studentList.addNewStudent("6610451249", "Ariyachai", 80.0);
+        Student student = studentList.findStudentById("6610451249");
+        assertNotNull(student);
+        assertEquals("Ariyachai", student.getName());
+        assertEquals(80.0, student.getScore());
+    }
+
+    @Test
+    void testGiveScoreToId() {
+        studentList.addNewStudent("6610451249", "Ariyachai");
+        studentList.giveScoreToId("6610451249", 90.0);
+        Student student = studentList.findStudentById("6610451249");
+        assertNotNull(student);
+        assertEquals(90.0, student.getScore());
+    }
+
+    @Test
+    void testViewGradeOfId() {
+        studentList.addNewStudent("6610451249", "Ariyachai", 85.0);
+        String grade = studentList.viewGradeOfId("6610451249");
+        assertNotNull(grade);
+        assertEquals("A", grade);
+    }
+
+    @Test
+    void testFindStudentById() {
+        studentList.addNewStudent("6610451249", "Ariyachai");
+        Student student = studentList.findStudentById("6610451249");
+        assertNotNull(student);
+        assertEquals("Ariyachai", student.getName());
+    }
+
+    @Test
+    void testGetStudents() {
+        studentList.addNewStudent("6610451249", "Ariyachai");
+        studentList.addNewStudent("6610400099", "Parindar");
+        assertEquals(2, studentList.getStudents().size());
+    }
+
 
 }
--- src/test/java/ku/cs/models/UserListTest.java
+++ src/test/java/ku/cs/models/UserListTest.java
@@ -11,46 +11,73 @@ class UserListTest {
     @DisplayName("User should be found in UserList")
     public void testUserListFindUser() {
         // TODO: add 3 users to UserList
+        UserList userList = new UserList();
+        userList.addUser("Tonnam1", "Inwza1");
+        userList.addUser("Tonnam2", "Inwza2");
+        userList.addUser("Tonnam3", "Inwza3");
 
         // TODO: find one of them
+        User user = userList.findUserByUsername("Tonnam1");
 
         // TODO: assert that UserList found User
-        // String expected = "<one of username>";
-        // String actual = user.getUsername();
-        // assertEquals(expected, actual);
+        String expected = "Tonnam1";
+        String actual = user.getUsername();
+        assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("User can change password")
     public void testUserCanChangePassword() {
         // TODO: add 3 users to UserList
+        UserList userList = new UserList();
+        userList.addUser("Tonnam1", "Inwza1");
+        userList.addUser("Tonnam2", "Inwza2");
+        userList.addUser("Tonnam3", "Inwza3");
 
         // TODO: change password of one user
+        boolean result = userList.changePassword("Tonnam1", "Inwza1", "Inwza001");
+        assertTrue(result);
 
         // TODO: assert that user can change password
-        // assertTrue(actual);
+        User user = userList.findUserByUsername("Tonnam1");
+        assertNotNull(user);
+        assertTrue(user.validatePassword("Inwza001"));
+        assertFalse(user.validatePassword("Inwza1"));
     }
 
     @Test
     @DisplayName("User with correct password can login")
     public void testUserListShouldReturnObjectIfUsernameAndPasswordIsCorrect() {
         // TODO: add 3 users to UserList
+        UserList userList = new UserList();
+        userList.addUser("Tonnam1", "Inwza1");
+        userList.addUser("Tonnam2", "Inwza2");
+        userList.addUser("Tonnam3", "Inwza3");
 
         // TODO: call login() with correct username and password
+        User user = userList.login("Tonnam1", "Inwza1");
 
         // TODO: assert that User object is found
-        // assertEquals(expected, actual);
+        String expected = "Tonnam1";
+        String actual = user.getUsername();
+        assertNotNull(user);
+        assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("User with incorrect password cannot login")
     public void testUserListShouldReturnNullIfUsernameAndPasswordIsIncorrect() {
         // TODO: add 3 users to UserList
+        UserList userList = new UserList();
+        userList.addUser("Tonnam1", "Inwza1");
+        userList.addUser("Tonnam2", "Inwza2");
+        userList.addUser("Tonnam3", "Inwza3");
 
         // TODO: call login() with incorrect username or incorrect password
+        User actual = userList.login("Tonnam1", "Inwza2");
 
         // TODO: assert that the method return null
-        // assertNull(actual);
+        assertNull(actual);
     }
 
 }
